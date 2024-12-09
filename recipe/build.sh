2e746b3e01f8465d2c29fc7ddc4f0600bdd5a066
Aaron Meurer <asmeurer@gmail.com>
2024-12-09 15:29:01 -0700

    Try to fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+
+export GLIB_CFLAGS="-I$PREFIX/include/glib-2.0 -I$PREFIX/lib/glib-2.0/include"
+export GLIB_LIBS="-L$PREFIX/lib -lglib-2.0 -lgobject-2.0 -lgmodule-2.0 -lgio-2.0"
+
 autoreconf -vfi
 ./configure --prefix=$PREFIX \
   --disable-static \
   --enable-relocatable \
   --disable-vala \
-  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH \
   CFLAGS="-I$PREFIX/include" \
   LDFLAGS="-L$PREFIX/lib"
 make
